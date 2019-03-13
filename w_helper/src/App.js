e780f158bc667e0a815ecadac94633cc069f31cb
kezhang <kezhang@Ctrip.com>
2019-03-13 15:54:09 +0800

    w_helper框架搭建

---
+++ w_helper/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd-mobile';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>xxxx</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
