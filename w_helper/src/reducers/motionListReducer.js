0c57e9330ed43331597643072e5f640e257c95f5
kezhang <kezhang@Ctrip.com>
2019-07-05 16:28:16 +0800

    motionList

--- w_helper/src/reducers/motionListReducer.js
+++ w_helper/src/reducers/motionListReducer.js
@@ -1,6 +1,13 @@
 import { SELECT_MOTION } from '../actions/constant';
 
-const _state = 0;
+const _state = [
+    {
+        motionId: 1,
+        motionName: '',
+        motionType: 1,
+        isJustice: true
+    }
+];
 
 export default function (state = _state, action) {
     switch (action.type) {
